95b4bbae0c1b0be8c0d1798b0184d6c6f8ed307b
NikolajK-HTX <nikolaj@jehoj.dk>
2020-04-24 22:51:50 +0200

    Added average calculation

--- arduino-code/src/main.cpp
+++ arduino-code/src/main.cpp
@@ -2,6 +2,10 @@
 #include <dht_nonblocking.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 
+float temperatures[6];
+float humidities[6];
+int counter = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -13,7 +17,7 @@ static bool measure_environment(float *temperature, float *humidity)
   static unsigned long measurement_timestamp = millis();
 
   /* Measure once every minute. */
-  if (millis() - measurement_timestamp > 60000ul)
+  if (millis() - measurement_timestamp > 10000ul)
   {
     if (dht_sensor.measure(temperature, humidity) == true)
     {
@@ -34,9 +38,18 @@ void loop() {
      true, then a measurement is available. */
   if (measure_environment(&temperature, &humidity) == true)
   {
-    Serial.print("T=");
-    Serial.print(temperature, 1);
-    Serial.print(";H=");
-    Serial.println(humidity, 1);
+    temperatures[counter] = temperature;
+    humidities[counter] = humidity;
+    counter++;
+    if (counter >= 6) {
+      counter = 0;
+      float averageTemperature = (temperatures[0]+temperatures[1]+temperatures[2]+temperatures[3]+temperatures[4]+temperatures[5])/6;
+      float averageHumidity = (humidities[0]+humidities[1]+humidities[2]+humidities[3]+humidities[4]+humidities[5])/6;
+      Serial.print("T=");
+      Serial.print(averageTemperature, 1);
+      Serial.print(";H=");
+      Serial.println(averageHumidity, 1);
+    }
+    
   }
 }
